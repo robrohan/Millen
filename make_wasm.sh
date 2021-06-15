397857c676407772fffdd35fccc0293f29c13cba
Rob Rohan <rob@therohans.com>
2021-06-15 19:11:59 +1200

    Adding my playing around game stuff

---
+++ make_wasm.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+make clean; 
+
+USE_POLYMOST=1 \
+WITHOUT_GTK=1 \
+USE_OPENGL=USE_GLES2 \
+USE_ASM=0 \
+GAME=freelancer \
+GAMEDATA=freelancer-data \
+CC=emcc \
+CXX=emcc \
+AR=emar \
+make 
